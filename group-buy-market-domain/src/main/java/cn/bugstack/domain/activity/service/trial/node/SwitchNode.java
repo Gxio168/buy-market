13cd8c4a53fc243dfe826fa1714b810a69786103
gxio168 <2632733615@qq.com>
2026-03-12 16:18:17 +0800

    feat: 完成DCC动态配置中心，完成降级和用户的切量

--- group-buy-market-domain/src/main/java/cn/bugstack/domain/activity/service/trial/node/SwitchNode.java
+++ group-buy-market-domain/src/main/java/cn/bugstack/domain/activity/service/trial/node/SwitchNode.java
@@ -1,10 +1,14 @@
 package cn.bugstack.domain.activity.service.trial.node;
 
+import cn.bugstack.domain.activity.adapter.repository.IActivityRepository;
 import cn.bugstack.domain.activity.model.entity.MarketProductEntity;
 import cn.bugstack.domain.activity.model.entity.TrialBalanceEntity;
 import cn.bugstack.domain.activity.service.trial.AbstractGroupBuyMarketSupport;
 import cn.bugstack.domain.activity.service.trial.factory.DefaultActivityStrategyFactory;
 import cn.bugstack.types.design.framework.tree.StrategyHandler;
+import cn.bugstack.types.enums.ResponseCode;
+import cn.bugstack.types.exception.AppException;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +20,26 @@ public class SwitchNode extends AbstractGroupBuyMarketSupport<MarketProductEntit
     @Resource
     private TagNode tagNode;
 
+    @Resource
+    private IActivityRepository repository;
+
     @Override
     public TrialBalanceEntity doApply(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        log.info("拼团商品查询试算服务-SwitchNode userId:{} requestParameter:{}", requestParameter.getUserId(), JSON.toJSONString(requestParameter));
+        String userId = requestParameter.getUserId();
+
+        // 判断是否降级
+        if (repository.downgradeSwitch()) {
+            log.info("拼团活动降级拦截 {}", userId);
+            throw new AppException(ResponseCode.E0003.getCode(), ResponseCode.E0003.getInfo());
+        }
+
+        // 切量范围判断
+        if (!repository.cutRange(userId)) {
+            log.info("拼团活动切量拦截 {}", userId);
+            throw new AppException(ResponseCode.E0004.getCode(), ResponseCode.E0004.getInfo());
+        }
+
         return router(requestParameter, dynamicContext);
     }
 
