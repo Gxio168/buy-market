84ce927a4cc061b7b15da4281baac79f36535baf
gxio168 <2632733615@qq.com>
2026-03-12 20:33:08 +0800

    feat: 完成责任链的构建

---
+++ group-buy-market-app/src/test/java/cn/bugstack/test/types/rule01/logic/RuleLogic101.java
@@ -0,0 +1,18 @@
+package cn.bugstack.test.types.rule01.logic;
+
+import cn.bugstack.test.types.rule01.factory.Rule01TradeRuleFactory;
+import cn.bugstack.types.design.framework.link.model1.AbstractLogicLink;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+public class RuleLogic101 extends AbstractLogicLink<String, Rule01TradeRuleFactory.DynamicContext, String> {
+
+    @Override
+    public String apply(String requestParameter, Rule01TradeRuleFactory.DynamicContext dynamicContext) throws Exception {
+        log.info("link model01 RuleLogic101");
+        return next(requestParameter, dynamicContext);
+    }
+}
---
+++ group-buy-market-app/src/test/java/cn/bugstack/test/types/rule01/logic/RuleLogic102.java
@@ -0,0 +1,19 @@
+package cn.bugstack.test.types.rule01.logic;
+
+
+import cn.bugstack.test.types.rule01.factory.Rule01TradeRuleFactory;
+import cn.bugstack.types.design.framework.link.model1.AbstractLogicLink;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RuleLogic102 extends AbstractLogicLink<String, Rule01TradeRuleFactory.DynamicContext, String> {
+
+    @Override
+    public String apply(String requestParameter, Rule01TradeRuleFactory.DynamicContext dynamicContext) {
+        log.info("link model01 RuleLogic102");
+
+        return "link model01 单实例链";
+    }
+}
